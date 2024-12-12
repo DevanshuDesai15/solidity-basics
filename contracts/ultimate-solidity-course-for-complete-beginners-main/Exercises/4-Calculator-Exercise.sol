00efa73bae8fc9ea0253714c9d4f2f72192893a5
DevanshuDesai15 <devanshudesai_1509@yahoo.com>
2024-12-12 16:05:56 -0600

    calculator

--- contracts/ultimate-solidity-course-for-complete-beginners-main/Exercises/4-Calculator-Exercise.sol
+++ contracts/ultimate-solidity-course-for-complete-beginners-main/Exercises/4-Calculator-Exercise.sol
@@ -8,19 +8,6 @@ pragma solidity ^0.8.0;
 // 4️⃣ Create a function to get result
 
 contract Calculator {
-    uint256 public result = 0;
+    uint256 public result;
 
-    function add(uint256 num) public {
-        result += num;
-    }
-    function sub(uint256 num) public {
-        result -= num;
-    }
-    function mul(uint256 num) public {
-        result *= num;
-    }
-
-    function get() public view returns(uint256){
-        return result;
-    }
 }
